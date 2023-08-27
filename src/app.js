2300d16d76bb92d89970d99a43ea862e3814a41f
Sikta Roy <siktaroy27@gmail.com>
2023-08-27 20:16:40 +0530

    Finishing touches

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+require("./db/patient");
+
+const port = process.env.PORT || 3000;
+
+const static_path = path.join(__dirname, "../public" );
+console.log(path.join(__dirname, "../public" ));
+app.use(express.static(static_path));
+
+app.get("/",(req,res) => {
+    res.send("wow")
+});
+
+app.listen(port,() => {
+    console.log(`server is running at port no ${port}`);
+})
--- src/db/patient.js
+++ src/db/patient.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/revival",{
+mongoose.connect("mongodb://127.0.0.1:27017/Revival",{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>{
