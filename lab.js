f1416a8fc006c3d436c6378d079f4d382b53d4b5
Sikta Roy <109263627+sikta-roy@users.noreply.github.com>
2023-08-27 10:10:48 +0530

    Update lab.js

--- lab.js
+++ lab.js
@@ -1,13 +1,1 @@
-document.getElementById("bookTestButton").addEventListener("click", () => {
-  const fullName = document.getElementById("fullName").value;
-  const testType = document.getElementById("testType").value;
 
-  const confirmation = confirm(`Are you sure you want to book a ${testType} Test for ${fullName}?`);
-
-  if (confirmation) {
-    console.log("Booking confirmed.");
-    alert("Booking confirmed. We will contact you soon."); // Display a confirmation message
-  } else {
-    console.log("Booking canceled.");
-  }
-});
